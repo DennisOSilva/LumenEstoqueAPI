8aec3c58604fff5bdb7125439e4dcd20f6634885
Dennis <dennis.sk257@gmail.com>
2026-04-21 18:43:29 -0300

    bug-fix

--- Validations/CnpjAttribute.cs
+++ Validations/CnpjAttribute.cs
@@ -1,15 +1,13 @@
 ﻿using System.ComponentModel.DataAnnotations;
-
 namespace LumenEstoque.Validations;
-
 public class CnpjAttribute : ValidationAttribute
 {
     protected override ValidationResult? IsValid(object? value, ValidationContext context)
     {
-        var cnpj = value.ToString()!.Replace(".", "").Replace("/", "").Replace("-", "");
-
         if (value is null || string.IsNullOrWhiteSpace(value.ToString()))
             return ValidationResult.Success;
+
+        var cnpj = value.ToString()!.Replace(".", "").Replace("/", "").Replace("-", "");
 
         if (cnpj.Length != 14 || cnpj.Distinct().Count() == 1)
             return new ValidationResult("CNPJ inválido");
@@ -28,18 +26,16 @@ public class CnpjAttribute : ValidationAttribute
         var sum = 0;
         for (int i = 0; i < 12; i++)
             sum += int.Parse(cnpj[i].ToString()) * multipliers1[i];
-
         var remainder = sum % 11;
         var digit1 = remainder < 2 ? 0 : 11 - remainder;
 
         sum = 0;
         for (int i = 0; i < 13; i++)
             sum += int.Parse(cnpj[i].ToString()) * multipliers2[i];
-
         remainder = sum % 11;
         var digit2 = remainder < 2 ? 0 : 11 - remainder;
 
-        return cnpj[12] == char.Parse(digit1.ToString()) &&
-               cnpj[13] == char.Parse(digit2.ToString());
+        return (cnpj[12] - '0') == digit1 &&
+               (cnpj[13] - '0') == digit2;
     }
 }
